cccd5d7de812d5fcdd9aa3a76cd3db8850a9e210
Christ ASSANA <christassana@gmail.com>
2024-06-20 01:18:30 +0200

    Ajoutez des fichiers projet.

---
+++ projEncapsulation/BeerEncapsulator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projEncapsulation
+{
+    public class BeerEncapsulator
+    {
+        public BeerEncapsulator(int avalaibleBottles, double avalaibleBeerVolume, int avalaibleCapsules)
+        {
+        private int _avalaibleBeerVolume;
+        int avalaibleBottles;
+        private int _avalaibleBottles = 15;
+        private int _avalaibleCapsule = 15;
+        public string wantedBottle = "";
+        public bool myBooleen = false;
+        }
+    
+
+        public double GetAvalableBeerVolume()
+        {
+            return _avalaibleBeerVolume;
+        }
+        public int GetAvailableBottles()
+        {  
+            return _avalaibleBottles; 
+        }
+
+        public double AdBeer()
+        {
+         Console.WriteLine("Combien de volumes en centililtres, voulez vous ?");
+          string stringX=Console.ReadLine();
+          double x=double.Parse(stringX);
+         _avalaibleBeerVolume = Math.Round(_avalaibleBeerVolume + x*0.33,2);
+          return _avalaibleBeerVolume;
+          }
+
+        }
+
+ }       
+}
+
+
+
---
+++ projEncapsulation/Program.cs
@@ -0,0 +1,30 @@
+﻿using projEncapsulation;
+using System;
+using System.Globalization;
+
+
+
+class MainProgram
+{
+    public static void Main(string[] args)
+    {
+        
+        Console.WriteLine(" Combien de litres de bière, avez vous ? ");
+        string stringLitresBeer = Console.ReadLine(); 
+        double LitresBeer = Double.Parse(stringLitresBeer);
+
+        Console.WriteLine(" Combien de bouteilles vides, avez vous ? ");
+        string stringEmptyBottles = Console.ReadLine();
+        double emptyBottles = Double.Parse(stringEmptyBottles);
+
+        Console.WriteLine("Combien de capsules, avez vous ? ");
+        string stringCapsules = Console.ReadLine();
+        double capsules = Double.Parse(stringEmptyBottles);
+
+        BeerEncapsulator beerEncapsulator = new BeerEncapsulator(LitresBeer,emptyBottles,capsules);
+
+    }
+
+
+
+}
